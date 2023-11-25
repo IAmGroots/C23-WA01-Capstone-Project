c249bbe8fbfafda922e1f7bcdea886de96ece139
Muchlis Abi <amanyu851@gmail.com>
2023-11-26 01:14:06 +0800

    Profile

--- app/src/main/java/com/example/capstoneproject/ui/articles/ArticlesFragment.kt
+++ app/src/main/java/com/example/capstoneproject/ui/articles/ArticlesFragment.kt
@@ -1,23 +1,14 @@
 package com.example.capstoneproject.ui.articles
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.capstoneproject.R
 import com.example.capstoneproject.adapter.ArticlesAdapter
 import com.example.capstoneproject.databinding.FragmentArticlesBinding
-import com.example.capstoneproject.model.Articles
-import com.example.capstoneproject.model.DataSourceArticles
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class ArticlesFragment : Fragment() {
 
