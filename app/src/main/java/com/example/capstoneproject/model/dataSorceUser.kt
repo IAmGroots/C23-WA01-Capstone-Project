452baf921811b71fd1bdceeea65205101e039f02
DindaNurAfifah <dindhana12@gmail.com>
2023-11-26 18:52:34 +0800

    commit

---
+++ app/src/main/java/com/example/capstoneproject/model/dataSorceUser.kt
@@ -0,0 +1,30 @@
+package com.example.capstoneproject.model
+
+object dataSorceUser {
+    val user = listOf(
+        dataUser(
+            "hadiseptian",
+            "",
+            1,
+            "a269bsy2325@bangkit.academy",
+            "nursukamto",
+            "hadi12345"
+        ),
+        dataUser(
+            "dinda",
+            "08115582588",
+            2,
+            "din@gmail.com",
+            "afifah",
+            "dinda12345"
+        ),
+        dataUser(
+            "muchlis",
+            "081350502003",
+            3,
+            "a269bsy2847@gmail.com",
+            "abimanyu",
+            "muchlis12345"
+        ),
+    )
+}
