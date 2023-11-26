58bc085794d585fb042a918247cd0431e7ed257e
DindaNurAfifah <dindhana12@gmail.com>
2023-11-26 23:34:02 +0800

    commit

---
+++ app/src/main/java/com/example/capstoneproject/ui/customView/mobileEditText.kt
@@ -0,0 +1,43 @@
+package com.example.capstoneproject.ui.customView
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class mobileEditText : AppCompatEditText {
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        init()
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttributeSet: Int) : super(context, attributeSet, defStyleAttributeSet) {
+        init()
+    }
+
+    private fun init() {
+        addTextChangedListener(object  : TextWatcher {
+            override fun beforeTextChanged(text: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(text: Editable?) {
+                if (!isPhoneNumberValid(text.toString()) || text.toString().length < 11) {
+                    setError("Invalid phone number")
+                } else {
+                    error = null
+                }
+            }
+        })
+    }
+
+    private fun isPhoneNumberValid(phoneNumber: String): Boolean {
+        val pattern = Regex("^(08|628)\\d{9,}$")
+        return pattern.matches(phoneNumber)
+    }
+}
