36b3ed7b4ba13088e9842f65312f9653c58bc834
Hadiseptian <hadiseptian.crb@gmail.com>
2023-12-01 17:46:01 +0800

    add maps & social media

---
+++ app/src/main/java/com/example/capstoneproject/adapter/HotspotAdapter.kt
@@ -0,0 +1,106 @@
+package com.example.capstoneproject.adapter
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.capstoneproject.databinding.HotspotItemsBinding
+import com.example.capstoneproject.model.Hotspot
+import com.example.capstoneproject.ui.articles.detail_article.DetailArticleActivity
+
+class HotspotAdapter(private val listHotspot: MutableList<Hotspot>) : RecyclerView.Adapter<HotspotAdapter.HotspotViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HotspotViewHolder {
+        val binding = HotspotItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HotspotViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HotspotViewHolder, position: Int) {
+        val hotspot = listHotspot[position]
+        holder.bind(hotspot)
+    }
+
+    override fun getItemCount(): Int {
+        return listHotspot.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> ViewType.FIRST_ITEM.ordinal
+            listHotspot.size - 1 -> ViewType.LAST_ITEM.ordinal
+            else -> ViewType.OTHER_ITEMS.ordinal
+        }
+    }
+
+//    private fun openMaps(lat: Double, lon: Double, name: String?) {
+//        val name = name ?: "$lat,$lon"
+//        val gmmIntentUri = Uri.parse("geo:$lat,$lon:?q=" + Uri.encode("$name"))
+//        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+//        mapIntent.setPackage("com.google.android.apps.maps")
+//        mapIntent.resolveActivity(this.packageManager)?.let {
+//            startActivity(mapIntent)
+//        }
+//    }
+
+    inner class HotspotViewHolder(private val binding: HotspotItemsBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(hotspot: Hotspot) {
+            binding.nameHotspot.text = hotspot.name
+            binding.locationHotspot.text = hotspot.location
+            Glide.with(binding.root)
+                .load(hotspot.img)
+                .into(binding.imgHotspot)
+
+            binding.btnDetail.setOnClickListener { item ->
+//                Toast.makeText(it.context, hotspot.name, Toast.LENGTH_SHORT).show()
+//                val gmmIntentUri = Uri.parse("geo:${hotspot.lat},${hotspot.lon}")
+//                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+//                mapIntent.setPackage("com.google.android.apps.maps")
+//                mapIntent.resolveActivity(item.context.packageManager)?.let {
+//                    item.context.startActivity(mapIntent)
+//                }
+
+                val gmmIntentUri = Uri.parse("google.navigation:q="+ hotspot.lat +","+ hotspot.lon + "&mode=w")
+                val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                item.context.startActivity(mapIntent)
+            }
+//                val detailArticle = Intent(it.context, DetailArticleActivity::class.java)
+//                detailArticle.putExtra(DetailArticleActivity.EXTRA_ARTICLE, article)
+//                it.context.startActivity(detailArticle)
+
+            // set margin for first and last item, and then margin for separator each item
+            val marginTopBottom = 0
+            val marginSeparator = 4
+            val density = binding.root.context.resources.displayMetrics.density
+            val topBottom = (marginTopBottom * density).toInt()
+            val separator = (marginSeparator * density).toInt()
+            val layoutParams = binding.root.layoutParams as RecyclerView.LayoutParams
+
+            when (getItemViewType(adapterPosition)) {
+                ViewType.FIRST_ITEM.ordinal -> {
+                    layoutParams.topMargin = topBottom
+                    layoutParams.bottomMargin = separator
+                }
+                ViewType.LAST_ITEM.ordinal -> {
+                    layoutParams.topMargin = separator
+                    layoutParams.bottomMargin = topBottom
+                }
+                else -> {
+                    layoutParams.topMargin = separator
+                    layoutParams.bottomMargin = separator
+                }
+            }
+
+            binding.root.layoutParams = layoutParams
+        }
+    }
+
+    enum class ViewType {
+        FIRST_ITEM,
+        LAST_ITEM,
+        OTHER_ITEMS
+    }
+}
---
+++ app/src/main/java/com/example/capstoneproject/model/Hotspot.kt
@@ -0,0 +1,10 @@
+package com.example.capstoneproject.model
+
+data class Hotspot(
+    val id: Int,
+    val name: String,
+    val location: String,
+    val img: String,
+    val lat: Double,
+    val lon: Double
+)
