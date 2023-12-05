d810773e0a9591fd1f9221a54dced8c6f16019e0
DindaNurAfifah <dindhana12@gmail.com>
2023-12-05 22:50:10 +0800

    commit

---
+++ app/src/main/java/com/example/capstoneproject/ui/login/resetPasswordActivity.kt
@@ -0,0 +1,68 @@
+package com.example.capstoneproject.ui.login
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Toast
+import com.example.capstoneproject.R
+import com.example.capstoneproject.databinding.ActivityResetPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class resetPasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResetPasswordBinding
+    private lateinit var auth: FirebaseAuth
+    var email: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResetPasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        setFocusable()
+
+        email = intent.getStringExtra("email").toString()
+        binding.etEmail.setText(email)
+
+        binding.btnSend.setOnClickListener {
+            val etEmail = binding.etEmail.text.toString()
+            if (binding.etEmail.text.toString().isEmpty()) {
+                Toast.makeText(this, "Enter the email", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.sendPasswordResetEmail(etEmail).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Check your email", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Send email failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setFocusable() {
+        binding.etEmail.setOnTouchListener { _, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    binding.etEmail.isFocusable = true
+                    binding.etEmail.isFocusableInTouchMode = true
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    binding.etEmail.requestFocus()
+                }
+            }
+            false
+        }
+    }
+}
